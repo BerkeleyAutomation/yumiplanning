695ad5e83fc572df926b9e576abf973784eaf2aa
Justin Kerr <justin_kerr@berkeley.edu>
2021-07-15 15:32:00 -0700

    added function for making a cartesian path over waypoints

--- yumiplanning/yumi_kinematics.py
+++ yumiplanning/yumi_kinematics.py
@@ -7,6 +7,9 @@ class YuMiKinematics():
              108.93368164,  75.65987325, 139.55185761]))
     R_NICE_STATE=np.array([ 1.21442839, -1.03205606, -1.10072738,  0.2987352,  
              -1.85257716,  1.25363652,-2.42181893])
+    L_NICE_POSE = RigidTransform(translation=(.5,.2,.256),rotation=[0,1,0,0],from_frame='gripper_l_base',to_frame='base_link')
+    R_NICE_POSE = RigidTransform(translation=(.5,-.2,.256),rotation=[0,1,0,0],from_frame='gripper_l_base',to_frame='base_link')
+    
     def __init__(self):
         '''
         Initializes the kinematics with the urdf file
@@ -23,10 +26,10 @@ class YuMiKinematics():
         % Manipulation2: runs for full timeout, returns solution that minimizes the ratio of min to max singular values of the Jacobian.
 '''
         self.left_solvers={}
-        self.left_solvers["Distance"]=TracIKSolver(urdf_path,self.base_frame,self.l_tip_frame,timeout=.05,solve_type="Distance")
+        self.left_solvers["Distance"]=TracIKSolver(urdf_path,self.base_frame,self.l_tip_frame,timeout=.04,solve_type="Distance")
         self.left_solvers["Manipulation1"] = TracIKSolver(urdf_path,self.base_frame,self.l_tip_frame,timeout=.05,solve_type="Manipulation1")
         self.right_solvers={}
-        self.right_solvers["Distance"]= TracIKSolver(urdf_path,self.base_frame,self.r_tip_frame,timeout=.05,solve_type="Distance")
+        self.right_solvers["Distance"]= TracIKSolver(urdf_path,self.base_frame,self.r_tip_frame,timeout=.04,solve_type="Distance")
         self.right_solvers["Manipulation1"] = TracIKSolver(urdf_path,self.base_frame,self.r_tip_frame,timeout=.2,solve_type="Manipulation1")
        
     def ik(self, left_pose=None, right_pose=None, left_qinit=None, right_qinit=None, solve_type="Distance"):
@@ -60,7 +63,7 @@ class YuMiKinematics():
         return (lres,rres)
 
     def compute_cartesian_path(self,l_start=None,l_goal=None,l_qinit=None,r_start=None,r_goal=None,r_qinit=None,
-            N=20,jump_thresh=.3):
+            N=10,jump_thresh=.3):
         '''
         Returns a traj interpolated linearly in cartesian space along the path between start and end
         all poses should be RigidTransform objects
@@ -82,6 +85,32 @@ class YuMiKinematics():
             if self.check_jumps(joint_traj,jump_thresh):
                print("Warning: jump detected in right traj")
             rres=joint_traj
+        return (lres,rres)
+
+    def interpolate_cartesian_waypoints(self,l_waypoints=None,r_waypoints=None,
+            l_qinit=None,r_qinit=None):
+        '''
+        Convenience function for chaining together a bunch of cartesian paths linearly
+        makes successive calls to compute_cartesian_path
+        '''
+        lres=None
+        rres=None
+        if l_waypoints is not None:
+            traj=[]
+            for i in range(len(l_waypoints)-1):
+                p,_=self.compute_cartesian_path(l_start=l_waypoints[i],l_goal=l_waypoints[i+1],
+                        l_qinit=l_qinit)
+                traj+=p
+                l_qinit=traj[-1]
+            lres=traj
+        if r_waypoints is not None:
+            traj=[]
+            for i in range(len(r_waypoints)-1):
+                _,p=self.compute_cartesian_path(r_start=r_waypoints[i],r_goal=r_waypoints[i+1],
+                        r_qinit=r_qinit)
+                traj+=p
+                r_qinit=traj[-1]
+            rres=traj
         return (lres,rres)
 
     def compute_traj(self, solvers, waypoints, jump_thresh,qinit=None):
@@ -143,19 +172,25 @@ class YuMiKinematics():
 
 if __name__=="__main__":
     yk = YuMiKinematics()
-    L_HOME_POSE = RigidTransform(translation=(.5,.2,.256),rotation=[0,1,0,0],from_frame='gripper_l_base',to_frame='base_link')
-    R_HOME_POSE = RigidTransform(translation=(.5,-.2,.256),rotation=[0,1,0,0],from_frame='gripper_l_base',to_frame='base_link')
-    left = RigidTransform(translation=(.5,-.1,.256),rotation=[0,1,0,0],from_frame='gripper',to_frame='world')
-    reoriented = RigidTransform(translation=(.5,-.1,.256),rotation=[0.0018,-.85,-.464,-.249],from_frame='gripper',to_frame='world')
-    # lpath, rpath = yk.compute_cartesian_path(l_start=L_HOME_POSE,l_goal=reoriented,l_qinit=YuMiKinematics.L_NICE_STATE)
-    # print("l_waypoints=[")
-    # for q in lpath:
-    #     y=YuMiKinematics.urdf_order_2_yumi(np.rad2deg(q))
-    #     print("[%f,%f,%f,%f,%f,%f,%f],"%(y[0],y[1],y[2],y[3],y[4],y[5],y[6]))
-    # print("]")
+    L_TWIST_POSE = RigidTransform(translation=(.35,.1,.18),rotation=[0.2439,.95426,.16741,.04325],from_frame='gripper',to_frame='world')
+    R_TWIST_POSE = RigidTransform(translation=(.35,-.12,.18),rotation=[0.3776,-.92516,.02571,-.02827],from_frame='gripper',to_frame='world')
+    L_OUT_POSE=RigidTransform(translation=(.35,.4,.3),rotation=[0,1,0,0],from_frame='gripper_l_base',to_frame='base_link')
+    R_OUT_POSE=RigidTransform(translation=(.35,-.3,.3),rotation=[0,1,0,0],from_frame='gripper',to_frame='world')
+
+    # lpath, rpath = yk.compute_cartesian_path(l_start=YuMiKinematics.L_NICE_POSE,
+    #     l_goal=L_TWIST_POSE,l_qinit=YuMiKinematics.L_NICE_STATE,
+    #     r_start=YuMiKinematics.R_NICE_POSE,r_goal=R_TWIST_POSE,r_qinit=YuMiKinematics.R_NICE_STATE)
+    lpts=[YuMiKinematics.L_NICE_POSE,L_TWIST_POSE,L_OUT_POSE,YuMiKinematics.L_NICE_POSE]
+    rpts=[YuMiKinematics.R_NICE_POSE,R_TWIST_POSE,R_OUT_POSE,YuMiKinematics.R_NICE_POSE]
+    lpath,rpath=yk.interpolate_cartesian_waypoints(lpts,rpts,YuMiKinematics.L_NICE_STATE,
+                   YuMiKinematics.R_NICE_STATE)
+    print("l_waypoints=[")
+    for q in lpath:
+        y=YuMiKinematics.urdf_order_2_yumi(np.rad2deg(q))
+        print("[%f,%f,%f,%f,%f,%f,%f],"%(y[0],y[1],y[2],y[3],y[4],y[5],y[6]))
+    print("]")
 
     #right arm stuff
-    lpath, rpath = yk.compute_cartesian_path(r_start=R_HOME_POSE,r_goal=reoriented,r_qinit=YuMiKinematics.R_NICE_STATE)
     print("r_waypoints=[")
     for q in rpath:
         y=YuMiKinematics.urdf_order_2_yumi(np.rad2deg(q))
