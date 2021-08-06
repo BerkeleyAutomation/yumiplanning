8d14350eb5529cddce9d545a0fc941f699600c01
Justin Kerr <justin_kerr@berkeley.edu>
2021-08-06 15:44:30 -0700

    add joint limits

--- ompl_planner/test_ompl.cpp
+++ ompl_planner/test_ompl.cpp
@@ -8,6 +8,7 @@
 #include <vector>
 #include <fcl/BVH/BVH_model.h>
 #include <fcl/collision.h>
+#include <fcl/shape/geometric_shapes.h>
 #include "stl_reader.h"
 #include <unordered_map>
 namespace YuMiPlanning{
@@ -38,13 +39,42 @@ public:
                 }
             }
         }
+        //initialize the table object
+        double h=.5;
+        std::shared_ptr<fcl::Box> table=std::make_shared<fcl::Box>(2,2,.5);
+        fcl::Vec3f T(0,0,-h/2);
+        tableObj = std::make_shared<fcl::CollisionObject>(table);
+        tableObj->setTranslation(T);
+        l_joints=getJoints(L_TIP);
+        r_joints=getJoints(R_TIP);
+    }
+    void getLeftJointLims(std::vector<double> &lower,std::vector<double> &upper){
+        lower.clear();
+        upper.clear();
+        for(auto j:l_joints){
+            lower.push_back(j->limits->lower);
+            upper.push_back(j->limits->upper);
+        }
+    }
+    void getRightJointLims(std::vector<double> &lower,std::vector<double> &upper){
+        lower.clear();
+        upper.clear();
+        for(auto j:r_joints){
+            lower.push_back(j->limits->lower);
+            upper.push_back(j->limits->upper);
+        }
     }
     /*
-        Returns true if the arm is colliding with itself (for now doesn't include inter-arm collisions)
+        Returns true if the arm is colliding with itself 
+        //TODO add inter-arm collisions
     */
     bool isColliding(std::vector<double> l_joints,std::vector<double> r_joints){
         return isColliding(l_joints,L_TIP) || isColliding(r_joints,R_TIP);
     }
+    /*
+        returns true if the arm from base link to tip frame is colliding with itself
+        //TODO add table collisions
+    */
     bool isColliding(std::vector<double>joints, std::string tip_frame){
         KDL::Chain chain;
         tree.getChain(BASE,tip_frame,chain);
@@ -73,12 +103,29 @@ public:
                 if(res.isCollision())return true;
             }   
         }
-        return false;
+        //check last object collision with table
+        fcl::CollisionRequest req;
+        fcl::CollisionResult res;
+        fcl::collide(objsToCheck[objsToCheck.size()-1].get(),tableObj.get(),req,res);
+        return res.isCollision();
     }
     const std::string L_TIP = "gripper_l_base";
     const std::string R_TIP = "gripper_r_base";
     const std::string BASE = "base_link";
 private:
+    std::vector<urdf::JointSharedPtr> getJoints(std::string tip_frame){
+        urdf::LinkConstSharedPtr tmp = robot_model.getLink(tip_frame);
+        std::vector<urdf::JointSharedPtr> js(7);
+        int i=6;
+        while(tmp->name!=BASE){
+            urdf::JointSharedPtr j=tmp->parent_joint;
+            if(j->type==urdf::Joint::REVOLUTE){
+                js[i--]=j;
+            }
+            tmp=robot_model.getLink(tmp->parent_joint->parent_link_name);
+        }
+        return js;
+    }
     ColObjPtr loadMesh(std::string filename){
         std::vector<fcl::Triangle> tris;
         std::vector<fcl::Vec3f> verts;
@@ -123,12 +170,25 @@ private:
     KDL::Tree tree;
     urdf::Model robot_model;
     std::unordered_map<std::string,ColObjPtr> objects;//dict that keeps track of all the loaded meshes
+    ColObjPtr tableObj;
+    std::vector<urdf::JointSharedPtr> l_joints;
+    std::vector<urdf::JointSharedPtr> r_joints;
 };
-}
+}//end namespace
 int main(){
     YuMiPlanning::CollisionChecker checker("/home/jkerr/yumi/yumiplanning/yumi_description/");
-    std::vector<double> zero(7,0.0);
-    bool collides = checker.isColliding(zero,zero);
+    std::vector<double> q={-1.2407575639621562, -1.4772790256426007, 0.9846829738212685, -0.5476077054575812, 2.1117175005455615, 1.2273630621015483, 3.2424116766608138};
+    bool collides = checker.isColliding(q,checker.L_TIP);
     std::cout<<collides<<std::endl;
+    std::vector<double> l;
+    std::vector<double> u;
+    checker.getLeftJointLims(l,u);
+    for(int i=0;i<7;i++){
+        std::cout<<"joint "<<i<<" limits: "<<l[i]<<" -> "<<u[i]<<std::endl;
+    }
+    checker.getRightJointLims(l,u);
+    for(int i=0;i<7;i++){
+        std::cout<<"joint "<<i<<" limits: "<<l[i]<<" -> "<<u[i]<<std::endl;
+    }
     return 0;
 }
